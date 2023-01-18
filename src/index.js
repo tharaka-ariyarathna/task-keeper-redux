3839d66fd64c2da24f9f74137cd49a3fb1eddc5a
Tharaka Ariyarathna <tharaka.ariyarathna@velaris.io>
2023-01-19 02:10:26 +0530

    created App function

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react" ;
+import Reactdom from 'react-dom/client' ;
+import App from './App' ;
+
+const root = Reactdom.createRoot(document.getElementById('root')) ;
+root.render(
+    <App />
+)
