6146dea2ea0f5325d3aa676d8a532ab60341e056
Tharaka Ariyarathna <tharaka.ariyarathna@velaris.io>
2023-01-19 12:48:28 +0530

    Completed addTask

--- src/components/show_task/showTask.js
+++ src/components/show_task/showTask.js
@@ -1,35 +1,32 @@
 import React from "react";
 import "./showTask.css";
 
-const ShowTask = ({taskList}) => { 
-    const taskListDummy = [
-    { title: "task1", description: "have to complete task 1" },
-    { title: "task2", description: "have to complete task 2" },
-    { title: "task3", description: "have to complete task 3" },
-  ];
-
-  if(taskList.length === 0){
-    console.log("working") ;
-  }
-
+const ShowTask = ({ taskList }) => {
   return (
-    <div className="showtask">
-      {taskListDummy.map((task) => {
-        return (
-          <div className="d-flex task m-3" style={{ alignItems: "center" }}>
-            <span>{task.title}</span>
-            <div style={{ display: "inline-block" }}>
-              <button type="button" className="btn btn-success btn-sm m-2 px-3">
-                Edit
-              </button>
-              <button type="button" className="btn btn-danger btn-sm m-2">
-                delete
-              </button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {taskList.length !== 0 && (
+        <div className="showtask">
+          {taskList.map((task) => {
+            return (
+              <div className="d-flex task m-3" style={{ alignItems: "center" }}>
+                <span>{task.title}</span>
+                <div style={{ display: "inline-block" }}>
+                  <button
+                    type="button"
+                    className="btn btn-success btn-sm m-2 px-3"
+                  >
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger btn-sm m-2">
+                    delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
